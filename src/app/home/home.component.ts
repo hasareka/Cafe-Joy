1a4db26e0e3400f6ea5dec8595502995c34b0eec
hasareka <hasarekat@gmail.com>
2023-08-15 08:50:06 +0530

    second

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  animations: [
-    trigger('fadeInOut', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('2s', style({ opacity: 1 })),
-      ]),
-      transition(':leave', [
-        animate('2s', style({ opacity: 0 }))
-      ])
-    ]),
-  ],
+  styleUrls: ['./home.component.css']
 })
-
 export class HomeComponent implements OnInit {
-  backgrounds = ['home1', 'home2', 'home3'];
+  backgrounds = [
+    { class: 'home1', text: 'Freshly Brewed Coffee' },
+    { class: 'home2', text: 'A Cozy Ambiance' },
+    { class: 'home3', text: 'Delicious Pastries' }
+  ];
+
   currentBackgroundIndex = 0;
+  previousBackgroundIndex = -1;
 
   constructor() { }
 
@@ -31,6 +24,28 @@ export class HomeComponent implements OnInit {
   }
 
   changeBackground(direction: number) {
+    this.previousBackgroundIndex = this.currentBackgroundIndex;
     this.currentBackgroundIndex = (this.currentBackgroundIndex + direction + this.backgrounds.length) % this.backgrounds.length;
+    
+    // Set animation based on direction
+    if (this.previousBackgroundIndex < this.currentBackgroundIndex || (this.previousBackgroundIndex === 2 && this.currentBackgroundIndex === 0)) {
+      this.setBackgroundAnimation('forward');
+    } else {
+      this.setBackgroundAnimation('backward');
+    }
+  }
+
+  setBackgroundAnimation(animationType: string) {
+    switch (this.backgrounds[this.currentBackgroundIndex].class) {
+      case 'home1':
+        this.backgrounds[this.currentBackgroundIndex].class = animationType === 'forward' ? 'home1' : 'home1';
+        break;
+      case 'home2':
+        this.backgrounds[this.currentBackgroundIndex].class = animationType === 'forward' ? 'home2' : 'home2';
+        break;
+      case 'home3':
+        this.backgrounds[this.currentBackgroundIndex].class = animationType === 'forward' ? 'home3' : 'home3';
+        break;
+    }
   }
 }
