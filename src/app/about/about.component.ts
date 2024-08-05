593e2776a7ba5545bdf8bc60a5d16df250824384
Thathsarani <hasarekat@gmail.com>
2024-08-05 22:40:42 +0530

    About us page modifications

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  showBanner = false;
 
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(event => {
+        const navigation = event as NavigationEnd;
+        this.route.fragment.subscribe(fragment => {
+          this.showBanner = (fragment === 'about');
+          if (this.showBanner) {
+            const element = document.getElementById('about-us-section');
+            if (element) {
+              element.scrollIntoView({ behavior: 'smooth' });
+            }
+          }
+        });
+      });
+  }
 }
